adb39704b0429f93d92299b6d90e40c579489ae5
wleader <william.leader@gmail.com>
2025-12-15 22:48:03 -0500

    Fix Namespace on LibraryPersistenceStore

--- OeSystems.ScriptableHttp/Scripting/LibraryPersistenceStore.cs
+++ OeSystems.ScriptableHttp/Scripting/LibraryPersistenceStore.cs
@@ -1,9 +1,8 @@
 using System;
 using System.Collections.Concurrent;
 using System.Linq;
-using OeSystems.ScriptableHttp.Scripting;
 
-namespace OeSystems.ScriptableHttp;
+namespace OeSystems.ScriptableHttp.Scripting;
 
 public interface ILibraryPersistenceStore
 {
