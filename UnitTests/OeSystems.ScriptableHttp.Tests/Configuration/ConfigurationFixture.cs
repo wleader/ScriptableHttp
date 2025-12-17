50dc82fe6f2c9f49d5926cef8f48b7d468f18b52
wleader <william.leader@gmail.com>
2025-12-16 21:47:31 -0500

    Remove unused code.

--- UnitTests/OeSystems.ScriptableHttp.Tests/Configuration/ConfigurationFixture.cs
+++ UnitTests/OeSystems.ScriptableHttp.Tests/Configuration/ConfigurationFixture.cs
@@ -1,8 +1,5 @@
 using System;
-using System.IO;
-using System.Reflection;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using OeSystems.ScriptableHttp.Configuration;
 using OeSystems.ScriptableHttp.Tests.Resources;
 
 namespace OeSystems.ScriptableHttp.Tests.Configuration;
@@ -10,26 +7,6 @@ namespace OeSystems.ScriptableHttp.Tests.Configuration;
 [TestClass]
 public class ScriptableHttpConfigFixture
 {
-    private static Assembly? _assembly;
-    private static string? _resourcePrefix;
-    
-    private static Stream? GetResourceStream(string resourceName)
-    {
-        _assembly ??= Assembly.GetExecutingAssembly();
-        _resourcePrefix ??= _assembly.GetName().Name + ".Configuration.";
-        var qualifiedName = _resourcePrefix + resourceName;
-        return _assembly.GetManifestResourceStream(qualifiedName);
-    }
-
-    private static ScriptableHttpConfig GetConfigurationFromResource(string resourceName)
-    {
-        using var stream = GetResourceStream(resourceName);
-        Assert.IsNotNull(stream, $"Could not find resource {resourceName}");
-        var result = ScriptableHttpConfig.Load(stream);
-        Assert.IsNotNull(result, $"Could not deserialize resource {resourceName}");
-        return result;
-    }
-    
     [TestMethod]
     public void Given_ConfigurationWithXmlRequestTemplate_When_Load_Then_Loaded()
     {
