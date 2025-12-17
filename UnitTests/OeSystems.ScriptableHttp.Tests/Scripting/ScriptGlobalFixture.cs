0cc66a1feff297b6831fc7e11f480ecd49f3f6a2
wleader <william.leader@gmail.com>
2025-12-16 21:49:12 -0500

    Stub out tests for .Scripting classes to act as a to-do reminder

---
+++ UnitTests/OeSystems.ScriptableHttp.Tests/Scripting/ScriptGlobalFixture.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace OeSystems.ScriptableHttp.Tests.Scripting;
+
+[TestClass]
+public class ScriptGlobalFixture
+{
+    [TestMethod]
+    public void When()
+    {
+        Assert.Inconclusive();
+    }
+}
---
+++ UnitTests/OeSystems.ScriptableHttp.Tests/Scripting/ScriptedOperationsFixture.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace OeSystems.ScriptableHttp.Tests.Scripting;
+
+[TestClass]
+public class ScriptedOperationsFixture
+{
+    [TestMethod]
+    public void When()
+    {
+        Assert.Inconclusive();
+    }
+}
